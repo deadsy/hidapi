7bcb1290d66e445eefe3659f22e33adc10117f0e
Jason Harris <sirmanlypowers@gmail.com>
2020-01-30 18:27:50 -0800

    Testing code.

--- test/main.go
+++ test/main.go
@@ -24,12 +24,39 @@ func libTest() error {
 		return err
 	}
 
-	devs := hidapi.Enumerate(0, 0)
+	//devs := hidapi.Enumerate(0, 0)
+	//for i, d := range devs {
+	//	fmt.Printf("Device %d\n", i)
+	//	fmt.Printf("%s\n", d)
+	//}
 
-	for i, d := range devs {
-		fmt.Printf("Device %d\n", i)
-		fmt.Printf("%s\n", d)
+	// Open the device using the VID, PID,
+	// and optionally the Serial number.
+
+	vid := uint16(0x046d)
+	pid := uint16(0xc52b)
+
+	dev := hidapi.Open(vid, pid, "")
+
+	s, err := dev.GetManufacturerString()
+	if err != nil {
+		return err
 	}
+	fmt.Printf("%s\n", s)
+
+	s, err = dev.GetProductString()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", s)
+
+	s, err = dev.GetSerialNumberString()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", s)
+
+	dev.Close()
 
 	err = hidapi.Exit()
 	if err != nil {
